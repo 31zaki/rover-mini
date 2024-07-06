b3ad1ce420879103ba0d4e57104dfb039b98c647
31zaki <zakisan.3141@gmail.com>
2024-07-07 03:31:56 +0900

    changes related to radio communications

--- codes/nRF24L01_arduino/nRF24L01_arduino.ino
+++ codes/nRF24L01_arduino/nRF24L01_arduino.ino
@@ -2,7 +2,6 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-// nRF24L01 pins
 #define CE_PIN   9
 #define CSN_PIN 10
 
@@ -12,15 +11,52 @@ const byte address[6] = "00001";
 
 void setup() {
   Serial.begin(9600);
-  radio.begin();
+  while (!Serial) {}
+
+  if (!radio.begin()) {
+    Serial.println("Radio hardware not responding!");
+    while (1) {}
+  }
+
+  Serial.println("Radio initialized.");
   radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+  radio.openReadingPipe(1, address);
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setDataRate(RF24_1MBPS);
+  radio.setChannel(0x60);
+  radio.startListening();  // Start in listening mode
+
+  // Print details for debugging
+  radio.printDetails();
 }
 
 void loop() {
   const char text[] = "Hello";
-  radio.write(&text, sizeof(text));
-  Serial.println("Sent: Hello");
+
+  // Sending
+  Serial.print("Sending: ");
+  Serial.println(text);
+  radio.stopListening();
+  bool report = radio.write(&text, sizeof(text));
+  if (report) {
+    Serial.println("Send successful");
+  } else {
+    Serial.println("Send failed");
+  }
+
+  // Switch to receive mode
+  radio.startListening();
+  delay(10);
+
+  // Receiving
+  if (radio.available()) {
+    char receivedText[32] = {0};
+    radio.read(&receivedText, sizeof(receivedText));
+    Serial.print("Received: ");
+    Serial.println(receivedText);
+  } else {
+    Serial.println("No data received");
+  }
+
   delay(1000);
 }
